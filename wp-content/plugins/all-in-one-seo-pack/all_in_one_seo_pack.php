0efddc20e5bc47ee9f4499e90193a52d08cfbf5f
fjuan <fjuan@splendeo.es>
2012-05-06 13:46:23 +0200

    update All in One Seo Pack plugin to version 1.6.14.2

--- wp-content/plugins/all-in-one-seo-pack/all_in_one_seo_pack.php
+++ wp-content/plugins/all-in-one-seo-pack/all_in_one_seo_pack.php
@@ -3,7 +3,7 @@
 Plugin Name: All in One SEO Pack
 Plugin URI: http://semperfiwebdesign.com
 Description: Out-of-the-box SEO for your WordPress blog. <a href="options-general.php?page=all-in-one-seo-pack/aioseop.class.php">Options configuration panel</a> | <a href="http://wpplugins.com/plugin/50/all-in-one-seo-pack-pro-version">Upgrade to Pro Version</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mrtorbert%40gmail%2ecom&item_name=All%20In%20One%20SEO%20Pack&item_number=Support%20Open%20Source&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8">Donate</a> | <a href="http://semperfiwebdesign.com/forum/" >Support</a> |  <a href="https://www.amazon.com/wishlist/1NFQ133FNCOOA/ref=wl_web" target="_blank" title="Amazon Wish List">Amazon Wishlist</a>
-Version: 1.6.14.2
+Version: 1.6.14.3
 Author: Michael Torbert
 Author URI: http://michaeltorbert.com
 */
@@ -30,11 +30,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * @package All-in-One-SEO-Pack
- * @version 1.6.14.2
+ * @version 1.6.14.3
  */
 
 if ( ! defined( 'AIOSEOP_VERSION' ) )
-    define( 'AIOSEOP_VERSION', '1.6.14.2' );
+    define( 'AIOSEOP_VERSION', '1.6.14.3' );
 
 if ( ! defined( 'AIOSEOP_PLUGIN_DIR' ) )
     define( 'AIOSEOP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
