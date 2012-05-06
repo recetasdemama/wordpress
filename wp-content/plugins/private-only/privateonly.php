2c074ff38db73725840767ad68930707edf76710
fjuan <fjuan@splendeo.es>
2012-05-06 15:22:20 +0200

    add private-only plugin

---
+++ wp-content/plugins/private-only/privateonly.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Plugin Name: Private Only
+Plugin URI: http://www.pixert.com/
+Description: Redirects all non-logged in users to login form with custom login capability
+Version: 2.5
+Author: Kate Mag (Pixel Insert)
+Author URI: http://www.pixert.com
+*/
+/*
+Copyright 2009 Kate Mag-Pixel Insert (email:studio[at]pixert dot com)
+This program is free softwate; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more detail
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Fondation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301 US
+*/
+?>
+<?php
+/* Localization */
+load_plugin_textdomain('private-only', false, basename( dirname( __FILE__ ) ) . '/languages' );
+//login page - sweet!
+//define constant folder to the plugin
+$wp_content_dir = ABSPATH . 'wp-content';
+$wp_plugin_dir = $wp_content_dir . '/plugins';
+define( 'PO_LOGIN', $wp_plugin_dir . '/private-only' );
+define( 'PO_LOGIN_URL', $wp_plugin_dir . '/private-only' );
+define( 'PO_LOGIN_ADMIN', $wp_plugin_dir . '/private-only/admin' );
+define( 'PO_LOGIN_CSS', $wp_plugin_dir . '/private-only/css' );
+	
+//load settings from database 
+if ( is_admin() )
+	require_once( PO_LOGIN_ADMIN . '/settings-admin.php' );
+	$po_login = get_option( 'po_login_settings' );
+  	function po_login_add_pages() {
+		if ( function_exists( 'add_options_page' ) ) 
+			$page=add_options_page( 'Private Only Custom Login', 'Private Only Custom Login', 'update_plugins' , 'privateonly.php', 'po_login_page');
+  	}
+  //main function
+  function po_custom_login() {
+	global $po_login;
+		echo '<!-- Private Only -->' . "\n\n";
+?>
+<?php if ( $po_login[ 'use_wp_logo' ] == true ) {} else { ?> 
+<?php if ( $po_login[ 'po_logo' ] != '' ) { ?>
+<style>
+#login h1 a{ background: url(<?php echo $po_login[ 'po_logo' ]; ?>) no-repeat top center !important; }
+</style>
+<?php } else { ?>
+<style>
+#login h1 a{ display: none !important; }
+</style>
+<?php } } ?>
+<?php
+		echo '<!-- Private Only by Kate Mag - @link: http://pixert.com-->' . "\n\n";
+  }
+  function po_login_plugin_actions($links, $file ) {
+ 	  if( $file == 'private-only/privateonly.php' && function_exists( "admin_url" ) ) {
+		  $settings_link = '<a href="' . admin_url( 'options-general.php?page=privateonly.php' ) . '">' . __('Private Only Custom Login') . '</a>';
+		  array_unshift( $links, $settings_link ); // before other links
+	}
+	return $links;
+  }
+  //add a settings page to menu
+	add_action( 'admin_menu', 'po_login_add_pages' );
+	add_action( 'login_head', 'po_custom_login' );
+	//Add a settings page to the plugin menu
+	add_filter( 'plugin_action_links', 'po_login_plugin_actions', 10, 2 );
+
+//Main Private Only features
+function private_only () {
+	$settings = get_option( 'po_login_settings' );
+	/* New Feature, code added by Ivan Ricotti. Thanks */
+	if (is_page($settings['public_pages'])) {
+		return;
+	}
+	if (!is_user_logged_in() && !is_feed()) {
+		auth_redirect();
+  } 
+}
+function no_index () {
+	echo "<meta name='robots' content='noindex,nofollow' />\n";
+}
+/* Fix Login Page Message */
+function custom_login_message() {
+$settings = get_option( 'po_login_settings' );
+$pagetitle = get_the_title($settings['public_pages']);
+$pagelink = get_permalink($settings['public_pages']);
+$message = '<p class="message">'.__('Only registered and logged in users are allowed to view this site. Please login now','private-only').'<br />';
+if (isset($settings['public_pages']) && $settings['public_pages']) {
+$message .= __('Visit our public page:','private-only') .'<a href='.$pagelink.'>'.$pagetitle.'</a></p>';
+}
+return $message;
+}
+add_filter('login_message', 'custom_login_message');
+function custom_register_message() {
+$message ='<p class="message">'.printf(__('Welcome,you need to be registered</a> to see content','private-only')).'</p><br />';
+return $message;
+}
+add_filter('register_message', 'custom_register_message');
+function log_in_message ($error) {
+	global $error;
+	$error="";
+	return $error;
+}
+/* Fix Logo Title */
+function change_wp_login_title() { 
+	echo '' . get_option('blogname'); 
+} 
+add_filter('login_headertitle', 'change_wp_login_title');
+add_action('template_redirect','private_only');
+add_action('login_head','no_index');
+add_filter('login_headertitle','log_in_message');
+?>
---
+++ wp-content/plugins/private-only/readme.txt
@@ -0,0 +1,31 @@
+===Private Only===
+Contributors: Kate Mag (Pixel Insert)
+Donate Link: http://www.pixert.com
+Tags: private access, login, remove feed
+Requires at least: 3.0
+Tested up to: 3.3.2
+Stable tag: 2.5
+
+Redirects all non-logged in users to login form.
+
+==Description==
+Do you have a private blog that you only want your friends and family to see and read it?
+This plugin will redirect all users who aren't logged in to the login form where they are shown a user-friendly message
+
+Features:
+1. Redirect all non-logged in users to login form
+2. Updated feature! Custom Logo on Login Page. You can change WordPress Logo
+3. Sub plugin name Disable Feed that allow you to choose disable or enable feed.
+4. Single public page *new feature*
+5. Localization *new feature*
+
+==Installation==
+Download The Private Only package file, extract the private-only directory from ZIP file, upload it to WordPress Plugins directory '/wp-content/plugins/'
+Activate Private Only via Plugin page, See Private Only, Disable Feed below the Private Only.
+See Settings tab on WP Admin, look for Private Only Custom Login settings
+
+If you are new to WordPress : http://codex.wordpress.org/Managing_Plugins#Installing_Plugins
+
+==Screenshot==
+Screnshots are available at http://pixert.com/blog
+
