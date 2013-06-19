af1eceded7ef78223602b8972497a491d1734b0c
Francisco de Juan <fjuan@splendeo.es>
2013-06-19 07:40:08 +0200

    update plugin wp-security-scan to version 3.1.0

--- wp-content/plugins/wp-security-scan/libs/wpssUtil.php
+++ wp-content/plugins/wp-security-scan/libs/wpssUtil.php
@@ -17,7 +17,7 @@ class wpssUtil
      */
     public static function displayDashboardWidget()
     {
-        //@since v3.0.8
+        // @since v2.0.6
         if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
             $opt = get_option('WSD-RSS-WGT-DISPLAY');
@@ -33,7 +33,7 @@ class wpssUtil
 
         //@ flag
         $run = false;
-        
+
         //@ check cache
         $optData = get_option('wsd_feed_data');
         if (! empty($optData))
@@ -65,7 +65,7 @@ class wpssUtil
         else { $run = true; }
 
         if (!$run) { return; }
-        
+
         $rss = fetch_feed(WPSS_WSD_BLOG_FEED);
 
         $out = '';
@@ -77,64 +77,65 @@ class wpssUtil
         }
         else
         {
-            // Limit to 5 entries. 
-            $maxitems = $rss->get_item_quantity(5); 
+            // Limit to 5 entries.
+            $maxitems = $rss->get_item_quantity(5);
 
             // Build an array of all the items,
-            $rss_items = $rss->get_items(0, $maxitems); 
+            $rss_items = $rss->get_items(0, $maxitems);
 
             $out .= '<ul>';
-                if ($maxitems == 0)
-                {
-                    $out.= '<li>'.__('There are no entries for this rss feed!').'</li>';
-                }
-                else
-                {
-                    foreach ( $rss_items as $item ) :
-                        $url = esc_url($item->get_permalink());
-                        $out.= '<li>';
-                            $out.= '<h4><a href="'.$url.'" target="_blank" title="Posted on '.$item->get_date('F j, Y | g:i a').'">';
-                                $out.= esc_html( $item->get_title() );
-                            $out.= '</a></h4>';
-                            $out.= '<p>';
-                                    $d = $item->get_description();
-                                    $p = substr($d, 0, 115).' <a href="'.$url.'" target="_blank" title="Read all article">[...]</a>';
-                                $out.= $p;
-                            $out.= '</p>';
-                        $out.= '</li>';
-                    endforeach;
-                }
+            if ($maxitems == 0)
+            {
+                $out.= '<li>'.__('There are no entries for this rss feed!').'</li>';
+            }
+            else
+            {
+                foreach ( $rss_items as $item ) :
+                    $url = esc_url($item->get_permalink());
+                    $out.= '<li>';
+                    $out.= '<h4><a href="'.$url.'" target="_blank" title="Posted on '.$item->get_date('F j, Y | g:i a').'">';
+                    $out.= esc_html( $item->get_title() );
+                    $out.= '</a></h4>';
+                    $out.= '<p>';
+                    $d = esc_html( $item->get_description());
+                    $p = substr($d, 0, 115).' <a href="'.$url.'" target="_blank" title="Read all article">[...]</a>';
+                    $out.= $p;
+                    $out.= '</p>';
+                    $out.= '</li>';
+                endforeach;
+            }
             $out.= '</ul>';
-            
-            $path = trailingslashit(get_option('siteurl')).'wp-content/plugins/wp-security-scan/';
+
+            $path = trailingslashit(get_option('siteurl')).'wp-content/plugins/secure-wordpress/';
 
             $out .= '<div style="border-top: solid 1px #ccc; margin-top: 4px; padding: 2px 0;">';
-                $out .= '<p style="margin: 5px 0 0 0; padding: 0 0; line-height: normal; overflow: hidden;">';
-                    $out .= '<a href="http://feeds.feedburner.com/Websitedefendercom"
-                                style="float: left; display: block; width: 50%; text-align: right; margin-top: 0; margin-left: 30px;
-                                padding-right: 22px; background: url('.$path.'images/rss.png) no-repeat right center;"
+            $out .= '<p style="margin: 5px 0 0 0; padding: 0 0; line-height: normal; overflow: hidden;">';
+            $out .= '<a href="http://feeds.feedburner.com/Websitedefendercom"
+                                style="float: left; display: block; width: 50%; text-align: right; margin-left: 30px;
+                                padding-right: 22px; background: url('.$path.'img/rss.png) no-repeat right center;"
                                 target="_blank">Follow us on RSS</a>';
-                    $out .= '<a href="#" id="wsd_close_rss_widget"
+            $out .= '<a href="#" id="wsd_close_rss_widget"
                                 style="float: right; display: block; width: 16px; height: 16px;
-                                margin: 0 0; background: url('.$path.'images/close-button.png) no-repeat 0 0;"
+                                margin: 0 0; background: url('.$path.'img/close-button.png) no-repeat 0 0;"
                                     title="Close widget"></a><form id="wsd_form" method="post"></form>';
-                $out .= '</p>';
-                $out .= '<script type="text/javascript">
+            $out .= '</p>';
+            $out .= '<script type="text/javascript">
                     document.getElementById("wsd_close_rss_widget").onclick = function(){
                             document.getElementById("wsd_form").submit();
                         };
                 </script>';
             $out .= '</div>';
         }
-        
+
         // Update cache
         $obj = new stdClass();
-            $obj->expires = time();
-            $obj->data = $out;
+        $obj->expires = time();
+        global $wpdb;
+        $obj->data = $wpdb->prepare($out);
         update_option('wsd_feed_data', $obj);
 
         echo $out;
-    } 
+    }
 
     /**
      * @public
@@ -152,7 +153,7 @@ class wpssUtil
                                     'wpssUtil::displayDashboardWidget');
         endif;
     }
-    
+
     /**
      * Hide the dashboard rss widget
      * @static
@@ -163,5 +164,5 @@ class wpssUtil
     {
         echo '<script>document.getElementById("'.self::$_pluginID.'").style.display = "none";</script>';
     }
-    
+
 }
