b8f1542b95e8da0cecfa4f9aa03eb806eadf63b9
Andrew Nacin <wp@andrewnacin.com>
2013-06-21 06:12:17 +0000

    Better validation of the URL used in core HTTP requests.
    Merges [24480] to the 3.5 branch.
    
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.5@24481 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-importer.php
+++ wp-admin/includes/class-wp-importer.php
@@ -5,44 +5,43 @@
 class WP_Importer {
 	/**
 	 * Class Constructor
+	 *
+	 * @return void
 	 */
-	public function __construct() {}
+	function __construct() {}
 
 	/**
 	 * Returns array with imported permalinks from WordPress database
 	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
-	 * @param string $importer_name
 	 * @param string $bid
 	 * @return array
 	 */
-	public function get_imported_posts( $importer_name, $bid ) {
+	function get_imported_posts( $importer_name, $bid ) {
 		global $wpdb;
 
 		$hashtable = array();
 
-		$limit  = 100;
+		$limit = 100;
 		$offset = 0;
 
 		// Grab all posts in chunks
 		do {
 			$meta_key = $importer_name . '_' . $bid . '_permalink';
-			$sql      = $wpdb->prepare( "SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key = %s LIMIT %d,%d", $meta_key, $offset, $limit );
-			$results  = $wpdb->get_results( $sql );
+			$sql = $wpdb->prepare( "SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key = '%s' LIMIT %d,%d", $meta_key, $offset, $limit );
+			$results = $wpdb->get_results( $sql );
 
 			// Increment offset
 			$offset = ( $limit + $offset );
 
-			if ( ! empty( $results ) ) {
+			if ( !empty( $results ) ) {
 				foreach ( $results as $r ) {
 					// Set permalinks into array
-					$hashtable[ $r->meta_value ] = intval( $r->post_id );
+					$hashtable[$r->meta_value] = intval( $r->post_id );
 				}
 			}
 		} while ( count( $results ) == $limit );
 
-		// Unset to save memory.
+		// unset to save memory
 		unset( $results, $r );
 
 		return $hashtable;
@@ -51,28 +50,24 @@ class WP_Importer {
 	/**
 	 * Return count of imported permalinks from WordPress database
 	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
-	 * @param string $importer_name
 	 * @param string $bid
 	 * @return int
 	 */
-	public function count_imported_posts( $importer_name, $bid ) {
+	function count_imported_posts( $importer_name, $bid ) {
 		global $wpdb;
 
 		$count = 0;
 
 		// Get count of permalinks
 		$meta_key = $importer_name . '_' . $bid . '_permalink';
-		$sql      = $wpdb->prepare( "SELECT COUNT( post_id ) AS cnt FROM $wpdb->postmeta WHERE meta_key = '%s'", $meta_key );
+		$sql = $wpdb->prepare( "SELECT COUNT( post_id ) AS cnt FROM $wpdb->postmeta WHERE meta_key = '%s'", $meta_key );
 
 		$result = $wpdb->get_results( $sql );
 
-		if ( ! empty( $result ) ) {
+		if ( !empty( $result ) )
 			$count = intval( $result[0]->cnt );
-		}
 
-		// Unset to save memory.
+		// unset to save memory
 		unset( $results );
 
 		return $count;
@@ -81,99 +76,80 @@ class WP_Importer {
 	/**
 	 * Set array with imported comments from WordPress database
 	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
 	 * @param string $bid
 	 * @return array
 	 */
-	public function get_imported_comments( $bid ) {
+	function get_imported_comments( $bid ) {
 		global $wpdb;
 
 		$hashtable = array();
 
-		$limit  = 100;
+		$limit = 100;
 		$offset = 0;
 
 		// Grab all comments in chunks
 		do {
-			$sql     = $wpdb->prepare( "SELECT comment_ID, comment_agent FROM $wpdb->comments LIMIT %d,%d", $offset, $limit );
+			$sql = $wpdb->prepare( "SELECT comment_ID, comment_agent FROM $wpdb->comments LIMIT %d,%d", $offset, $limit );
 			$results = $wpdb->get_results( $sql );
 
 			// Increment offset
 			$offset = ( $limit + $offset );
 
-			if ( ! empty( $results ) ) {
+			if ( !empty( $results ) ) {
 				foreach ( $results as $r ) {
 					// Explode comment_agent key
 					list ( $ca_bid, $source_comment_id ) = explode( '-', $r->comment_agent );
-					$source_comment_id                   = intval( $source_comment_id );
+					$source_comment_id = intval( $source_comment_id );
 
 					// Check if this comment came from this blog
 					if ( $bid == $ca_bid ) {
-						$hashtable[ $source_comment_id ] = intval( $r->comment_ID );
+						$hashtable[$source_comment_id] = intval( $r->comment_ID );
 					}
 				}
 			}
 		} while ( count( $results ) == $limit );
 
-		// Unset to save memory.
+		// unset to save memory
 		unset( $results, $r );
 
 		return $hashtable;
 	}
 
-	/**
-	 * @param int $blog_id
-	 * @return int|void
-	 */
-	public function set_blog( $blog_id ) {
+	function set_blog( $blog_id ) {
 		if ( is_numeric( $blog_id ) ) {
 			$blog_id = (int) $blog_id;
 		} else {
 			$blog = 'http://' . preg_replace( '#^https?://#', '', $blog_id );
-			if ( ( ! $parsed = parse_url( $blog ) ) || empty( $parsed['host'] ) ) {
+			if ( ( !$parsed = parse_url( $blog ) ) || empty( $parsed['host'] ) ) {
 				fwrite( STDERR, "Error: can not determine blog_id from $blog_id\n" );
 				exit();
 			}
-			if ( empty( $parsed['path'] ) ) {
+			if ( empty( $parsed['path'] ) )
 				$parsed['path'] = '/';
-			}
-			$blogs = get_sites(
-				array(
-					'domain' => $parsed['host'],
-					'number' => 1,
-					'path'   => $parsed['path'],
-				)
-			);
-			if ( ! $blogs ) {
+			$blog = get_blog_details( array( 'domain' => $parsed['host'], 'path' => $parsed['path'] ) );
+			if ( !$blog ) {
 				fwrite( STDERR, "Error: Could not find blog\n" );
 				exit();
 			}
-			$blog    = array_shift( $blogs );
 			$blog_id = (int) $blog->blog_id;
 		}
 
 		if ( function_exists( 'is_multisite' ) ) {
-			if ( is_multisite() ) {
+			if ( is_multisite() )
 				switch_to_blog( $blog_id );
-			}
 		}
 
 		return $blog_id;
 	}
 
-	/**
-	 * @param int $user_id
-	 * @return int|void
-	 */
-	public function set_user( $user_id ) {
+	function set_user( $user_id ) {
 		if ( is_numeric( $user_id ) ) {
 			$user_id = (int) $user_id;
 		} else {
 			$user_id = (int) username_exists( $user_id );
 		}
 
-		if ( ! $user_id || ! wp_set_current_user( $user_id ) ) {
+		if ( !$user_id || !wp_set_current_user( $user_id ) ) {
 			fwrite( STDERR, "Error: can not find user\n" );
 			exit();
 		}
@@ -188,7 +164,7 @@ class WP_Importer {
 	 * @param string $b
 	 * @return int
 	 */
-	public function cmpr_strlen( $a, $b ) {
+	function cmpr_strlen( $a, $b ) {
 		return strlen( $b ) - strlen( $a );
 	}
 
@@ -198,25 +174,24 @@ class WP_Importer {
 	 * @param string $url
 	 * @param string $username
 	 * @param string $password
-	 * @param bool   $head
+	 * @param bool $head
 	 * @return array
 	 */
-	public function get_page( $url, $username = '', $password = '', $head = false ) {
+	function get_page( $url, $username = '', $password = '', $head = false ) {
 		// Increase the timeout
-		add_filter( 'http_request_timeout', array( $this, 'bump_request_timeout' ) );
+		add_filter( 'http_request_timeout', array( &$this, 'bump_request_timeout' ) );
 
 		$headers = array();
-		$args    = array();
-		if ( true === $head ) {
+		$args = array();
+		$args['reject_unsafe_urls'] = true;
+		if ( true === $head )
 			$args['method'] = 'HEAD';
-		}
-		if ( ! empty( $username ) && ! empty( $password ) ) {
+		if ( !empty( $username ) && !empty( $password ) )
 			$headers['Authorization'] = 'Basic ' . base64_encode( "$username:$password" );
-		}
 
 		$args['headers'] = $headers;
 
-		return wp_safe_remote_request( $url, $args );
+		return wp_remote_request( $url, $args );
 	}
 
 	/**
@@ -225,7 +200,7 @@ class WP_Importer {
 	 * @param int $val
 	 * @return int
 	 */
-	public function bump_request_timeout( $val ) {
+	function bump_request_timeout( $val ) {
 		return 60;
 	}
 
@@ -234,9 +209,10 @@ class WP_Importer {
 	 *
 	 * @return bool
 	 */
-	public function is_user_over_quota() {
+	function is_user_over_quota() {
 		if ( function_exists( 'upload_is_user_over_quota' ) ) {
-			if ( upload_is_user_over_quota() ) {
+			if ( upload_is_user_over_quota( 1 ) ) {
+				echo "Sorry, you have used your upload quota.\n";
 				return true;
 			}
 		}
@@ -250,19 +226,16 @@ class WP_Importer {
 	 * @param string $string
 	 * @return string
 	 */
-	public function min_whitespace( $string ) {
+	function min_whitespace( $string ) {
 		return preg_replace( '|[\r\n\t ]+|', ' ', $string );
 	}
 
 	/**
-	 * Resets global variables that grow out of control during imports.
+	 * Reset global variables that grow out of control during imports
 	 *
-	 * @since 3.0.0
-	 *
-	 * @global wpdb  $wpdb       WordPress database abstraction object.
-	 * @global array $wp_actions
+	 * @return void
 	 */
-	public function stop_the_insanity() {
+	function stop_the_insanity() {
 		global $wpdb, $wp_actions;
 		// Or define( 'WP_IMPORTING', true );
 		$wpdb->queries = array();
@@ -276,54 +249,51 @@ class WP_Importer {
  * Exits when a required param is not set.
  *
  * @param string $param
- * @param bool   $required
+ * @param bool $required
  * @return mixed
  */
 function get_cli_args( $param, $required = false ) {
 	$args = $_SERVER['argv'];
-	if ( ! is_array( $args ) ) {
-		$args = array();
-	}
 
 	$out = array();
 
 	$last_arg = null;
-	$return   = null;
+	$return = null;
 
 	$il = sizeof( $args );
 
 	for ( $i = 1, $il; $i < $il; $i++ ) {
-		if ( (bool) preg_match( '/^--(.+)/', $args[ $i ], $match ) ) {
-			$parts = explode( '=', $match[1] );
-			$key   = preg_replace( '/[^a-z0-9]+/', '', $parts[0] );
+		if ( (bool) preg_match( "/^--(.+)/", $args[$i], $match ) ) {
+			$parts = explode( "=", $match[1] );
+			$key = preg_replace( "/[^a-z0-9]+/", "", $parts[0] );
 
 			if ( isset( $parts[1] ) ) {
-				$out[ $key ] = $parts[1];
+				$out[$key] = $parts[1];
 			} else {
-				$out[ $key ] = true;
+				$out[$key] = true;
 			}
 
 			$last_arg = $key;
-		} elseif ( (bool) preg_match( '/^-([a-zA-Z0-9]+)/', $args[ $i ], $match ) ) {
+		} else if ( (bool) preg_match( "/^-([a-zA-Z0-9]+)/", $args[$i], $match ) ) {
 			for ( $j = 0, $jl = strlen( $match[1] ); $j < $jl; $j++ ) {
-				$key         = $match[1]{$j};
-				$out[ $key ] = true;
+				$key = $match[1]{$j};
+				$out[$key] = true;
 			}
 
 			$last_arg = $key;
-		} elseif ( $last_arg !== null ) {
-			$out[ $last_arg ] = $args[ $i ];
+		} else if ( $last_arg !== null ) {
+			$out[$last_arg] = $args[$i];
 		}
 	}
 
 	// Check array for specified param
-	if ( isset( $out[ $param ] ) ) {
+	if ( isset( $out[$param] ) ) {
 		// Set return value
-		$return = $out[ $param ];
+		$return = $out[$param];
 	}
 
 	// Check for missing required param
-	if ( ! isset( $out[ $param ] ) && $required ) {
+	if ( !isset( $out[$param] ) && $required ) {
 		// Display message and exit
 		echo "\"$param\" parameter is required but was not specified\n";
 		exit();
--- wp-includes/rss.php
+++ wp-includes/rss.php
@@ -10,21 +10,17 @@
  *
  * @package External
  * @subpackage MagpieRSS
- * @deprecated 3.0.0 Use SimplePie instead.
  */
 
 /**
  * Deprecated. Use SimplePie (class-simplepie.php) instead.
  */
-_deprecated_file( basename( __FILE__ ), '3.0.0', WPINC . '/class-simplepie.php' );
+_deprecated_file( basename( __FILE__ ), '3.0', WPINC . '/class-simplepie.php' );
 
-/**
- * Fires before MagpieRSS is loaded, to optionally replace it.
- *
- * @since 2.3.0
- * @deprecated 3.0.0
+/*
+ * Hook to use another RSS object instead of MagpieRSS
  */
-do_action( 'load_feed_engine' );
+do_action('load_feed_engine');
 
 /** RSS feed constant. */
 define('RSS', 'RSS');
@@ -55,18 +51,17 @@ class MagpieRSS {
 
 	var $_CONTENT_CONSTRUCTS = array('content', 'summary', 'info', 'title', 'tagline', 'copyright');
 
-	/**
-	 * PHP5 constructor.
-	 */
-	function __construct( $source ) {
+	function MagpieRSS ($source) {
 
-		# Check if PHP xml isn't compiled
+		# if PHP xml isn't compiled in, die
 		#
-		if ( ! function_exists('xml_parser_create') ) {
-			return trigger_error( "PHP's XML extension is not available. Please contact your hosting provider to enable PHP's XML extension." );
-		}
+		if ( !function_exists('xml_parser_create') )
+			trigger_error( "Failed to load PHP's XML Extension. http://www.php.net/manual/en/ref.xml.php" );
 
-		$parser = xml_parser_create();
+		$parser = @xml_parser_create();
+
+		if ( !is_resource($parser) )
+			trigger_error( "Failed to create an instance of PHP's XML parser. http://www.php.net/manual/en/ref.xml.php");
 
 		$this->parser = $parser;
 
@@ -98,13 +93,6 @@ class MagpieRSS {
 		$this->normalize();
 	}
 
-	/**
-	 * PHP4 constructor.
-	 */
-	public function MagpieRSS( $source ) {
-		self::__construct( $source );
-	}
-
 	function feed_start_element($p, $element, &$attrs) {
 		$el = $element = strtolower($element);
 		$attrs = array_change_key_case($attrs, CASE_LOWER);
@@ -112,7 +100,7 @@ class MagpieRSS {
 		// check for a namespace, and split if found
 		$ns	= false;
 		if ( strpos( $element, ':' ) ) {
-			list($ns, $el) = explode( ':', $element, 2);
+			list($ns, $el) = split( ':', $element, 2);
 		}
 		if ( $ns and $ns != 'rdf' ) {
 			$this->current_namespace = $ns;
@@ -255,7 +243,7 @@ class MagpieRSS {
 		}
 		elseif ($this->feed_type == ATOM and $this->incontent  ) {
 			// balance tags properly
-			// note: This may not actually be necessary
+			// note:  i don't think this is actually neccessary
 			if ( $this->stack[0] == $el )
 			{
 				$this->append_content("</$el>");
@@ -434,7 +422,7 @@ function fetch_rss ($url) {
 	else {
 		// Flow
 		// 1. check cache
-		// 2. if there is a hit, make sure it's fresh
+		// 2. if there is a hit, make sure its fresh
 		// 3. if cached obj fails freshness check, fetch remote
 		// 4. if remote fails, return stale object, or error
 
@@ -548,7 +536,7 @@ endif;
  * @return Snoopy style response
  */
 function _fetch_remote_file($url, $headers = "" ) {
-	$resp = wp_safe_remote_request( $url, array( 'headers' => $headers, 'timeout' => MAGPIE_FETCH_TIME_OUT ) );
+	$resp = wp_remote_request($url, array('headers' => $headers, 'timeout' => MAGPIE_FETCH_TIME_OUT, 'reject_unsafe_urls' => true ));
 	if ( is_wp_error($resp) ) {
 		$error = array_shift($resp->errors);
 
@@ -587,8 +575,8 @@ function _fetch_remote_file($url, $headers = "" ) {
  * @package External
  * @subpackage MagpieRSS
  *
- * @param array $resp
- * @return MagpieRSS|bool
+ * @param unknown_type $resp
+ * @return unknown
  */
 function _response_to_rss ($resp) {
 	$rss = new MagpieRSS( $resp->results );
@@ -597,7 +585,7 @@ function _response_to_rss ($resp) {
 	if ( $rss && (!isset($rss->ERROR) || !$rss->ERROR) ) {
 
 		// find Etag, and Last-Modified
-		foreach ( (array) $resp->headers as $h) {
+		foreach( (array) $resp->headers as $h) {
 			// 2003-03-02 - Nicola Asuni (www.tecnick.com) - fixed bug "Undefined offset: 1"
 			if (strpos($h, ": ")) {
 				list($field, $val) = explode(": ", $h, 2);
@@ -717,10 +705,7 @@ class RSSCache {
 	var $MAX_AGE	= 43200;  		// when are files stale, default twelve hours
 	var $ERROR 		= '';			// accumulate error messages
 
-	/**
-	 * PHP5 constructor.
-	 */
-	function __construct( $base = '', $age = '' ) {
+	function RSSCache ($base='', $age='') {
 		$this->BASE_CACHE = WP_CONTENT_DIR . '/cache';
 		if ( $base ) {
 			$this->BASE_CACHE = $base;
@@ -731,18 +716,11 @@ class RSSCache {
 
 	}
 
-	/**
-	 * PHP4 constructor.
-	 */
-	public function RSSCache( $base = '', $age = '' ) {
-		self::__construct( $base, $age );
-	}
-
 /*=======================================================================*\
 	Function:	set
 	Purpose:	add an item to the cache, keyed on url
-	Input:		url from which the rss file was fetched
-	Output:		true on success
+	Input:		url from wich the rss file was fetched
+	Output:		true on sucess
 \*=======================================================================*/
 	function set ($url, $rss) {
 		$cache_option = 'rss_' . $this->file_name( $url );
@@ -755,7 +733,7 @@ class RSSCache {
 /*=======================================================================*\
 	Function:	get
 	Purpose:	fetch an item from the cache
-	Input:		url from which the rss file was fetched
+	Input:		url from wich the rss file was fetched
 	Output:		cached object on HIT, false on MISS
 \*=======================================================================*/
 	function get ($url) {
@@ -776,7 +754,7 @@ class RSSCache {
 	Function:	check_cache
 	Purpose:	check a url for membership in the cache
 				and whether the object is older then MAX_AGE (ie. STALE)
-	Input:		url from which the rss file was fetched
+	Input:		url from wich the rss file was fetched
 	Output:		cached object on HIT, false on MISS
 \*=======================================================================*/
 	function check_cache ( $url ) {
@@ -809,7 +787,7 @@ class RSSCache {
 /*=======================================================================*\
 	Function:	file_name
 	Purpose:	map url to location in cache
-	Input:		url from which the rss file was fetched
+	Input:		url from wich the rss file was fetched
 	Output:		a file name
 \*=======================================================================*/
 	function file_name ($url) {
