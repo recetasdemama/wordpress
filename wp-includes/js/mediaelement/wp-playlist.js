10dbf711226a1167d153c2b0171136db76ad466d
Andrew Nacin <wp@andrewnacin.com>
2014-05-02 17:02:16 +0000

    Add a compatibility layer in wp-playlist.js to avoid VM errors from MediaElement's plugin bridge in the TinyMCE views for playlists by suppressing playback for files whose mime-type is not supported in the user's browser natively.
    Merges [28171] to the 3.9 branch.
    
    props wonderboymusic.
    fixes #27892.
    
    Built from https://develop.svn.wordpress.org/branches/3.9@28247
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@28075 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/mediaelement/wp-playlist.js
+++ wp-includes/js/mediaelement/wp-playlist.js
@@ -1,22 +1,14 @@
-/* global _wpmejsSettings, MediaElementPlayer */
+/*globals window, document, jQuery, _, Backbone, _wpmejsSettings */
 
 (function ($, _, Backbone) {
-	'use strict';
+	"use strict";
 
-	/** @namespace wp */
-	window.wp = window.wp || {};
-
-	var WPPlaylistView = Backbone.View.extend(/** @lends WPPlaylistView.prototype */{
-		/**
-		 * @constructs
-		 *
-		 * @param {Object} options          The options to create this playlist view with.
-		 * @param {Object} options.metadata The metadata
-		 */
+	var WPPlaylistView = Backbone.View.extend({
 		initialize : function (options) {
 			this.index = 0;
 			this.settings = {};
-			this.data = options.metadata || $.parseJSON( this.$('script.wp-playlist-script').html() );
+			this.compatMode = $( 'body' ).hasClass( 'wp-admin' ) && $( '#content_ifr' ).length;
+			this.data = options.metadata || $.parseJSON( this.$('script').html() );
 			this.playerNode = this.$( this.data.type );
 
 			this.tracks = new Backbone.Collection( this.data.tracks );
@@ -35,12 +27,14 @@
 				this.renderTracks();
 			}
 
-			this.playerNode.attr( 'src', this.current.get( 'src' ) );
+			if ( this.isCompatibleSrc() ) {
+				this.playerNode.attr( 'src', this.current.get( 'src' ) );
+			}
 
 			_.bindAll( this, 'bindPlayer', 'bindResetPlayer', 'setPlayer', 'ended', 'clickTrack' );
 
 			if ( ! _.isUndefined( window._wpmejsSettings ) ) {
-				this.settings = _.clone( _wpmejsSettings );
+				this.settings.pluginPath = _wpmejsSettings.pluginPath;
 			}
 			this.settings.success = this.bindPlayer;
 			this.setPlayer();
@@ -53,7 +47,25 @@
 
 		bindResetPlayer : function (mejs) {
 			this.bindPlayer( mejs );
-			this.playCurrentSrc();
+			if ( this.isCompatibleSrc() ) {
+				this.playCurrentSrc();
+			}
+		},
+
+		isCompatibleSrc: function () {
+			var testNode;
+
+			if ( this.compatMode ) {
+				testNode = $( '<span><source type="' + this.current.get( 'type' ) + '" /></span>' );
+
+				if ( ! wp.media.mixin.isCompatible( testNode ) ) {
+					this.playerNode.removeAttr( 'src' );
+					this.playerNode.removeAttr( 'poster' );
+					return;
+				}
+			}
+
+			return true;
 		},
 
 		setPlayer: function (force) {
@@ -64,11 +76,15 @@
 			}
 
 			if (force) {
-				this.playerNode.attr( 'src', this.current.get( 'src' ) );
+				if ( this.isCompatibleSrc() ) {
+					this.playerNode.attr( 'src', this.current.get( 'src' ) );
+				}
 				this.settings.success = this.bindResetPlayer;
 			}
 
-			// This is also our bridge to the outside world.
+			/**
+			 * This is also our bridge to the outside world
+			 */
 			this.player = new MediaElementPlayer( this.playerNode.get(0), this.settings );
 		},
 
@@ -129,7 +145,8 @@
 				this.next();
 			} else {
 				this.index = 0;
-				this.setCurrent();
+				this.current = this.tracks.at( this.index );
+				this.loadCurrent();
 			}
 		},
 
@@ -151,7 +168,7 @@
 
 			if ( last !== current ) {
 				this.setPlayer( true );
-			} else {
+			} else if ( this.isCompatibleSrc() ) {
 				this.playerNode.attr( 'src', this.current.get( 'src' ) );
 				this.playCurrentSrc();
 			}
@@ -171,32 +188,13 @@
 		}
 	});
 
-	/**
-	 * Initialize media playlists in the document.
-	 *
-	 * Only initializes new playlists not previously-initialized.
-	 *
-	 * @since 4.9.3
-	 * @returns {void}
-	 */
-	function initialize() {
-		$( '.wp-playlist:not(:has(.mejs-container))' ).each( function() {
-			new WPPlaylistView( { el: this } );
-		} );
-	}
-
-	/**
-	 * Expose the API publicly on window.wp.playlist.
-	 *
-	 * @namespace wp.playlist
-	 * @since 4.9.3
-	 * @type {object}
-	 */
-	window.wp.playlist = {
-		initialize: initialize
-	};
-
-	$( document ).ready( initialize );
+    $(document).ready(function () {
+		if ( ! $( 'body' ).hasClass( 'wp-admin' ) || $( 'body' ).hasClass( 'about-php' ) ) {
+			$('.wp-playlist').each(function () {
+				return new WPPlaylistView({ el: this });
+			});
+		}
+    });
 
 	window.WPPlaylistView = WPPlaylistView;
 
