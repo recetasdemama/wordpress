c2393953867a45842484a92fa08bac6091d31bc9
Francisco de Juan <francisco.juan@gmail.com>
2014-08-15 10:14:48 +0200

    update plugin social-media-feather to version 1.6.7

--- wp-content/plugins/social-media-feather/synved-social/synved-social.php
+++ wp-content/plugins/social-media-feather/synved-social/synved-social.php
@@ -3,7 +3,7 @@
 Module Name: Synved Social
 Description: Social sharing and following tools
 Author: Synved
-Version: 1.6.5
+Version: 1.6.7
 Author URI: http://synved.com/
 License: GPLv2
 
@@ -18,8 +18,8 @@ In no event shall Synved Ltd. be liable to you or any third party for any direct
 
 
 define('SYNVED_SOCIAL_LOADED', true);
-define('SYNVED_SOCIAL_VERSION', 100060005);
-define('SYNVED_SOCIAL_VERSION_STRING', '1.6.5');
+define('SYNVED_SOCIAL_VERSION', 100060007);
+define('SYNVED_SOCIAL_VERSION_STRING', '1.6.7');
 
 define('SYNVED_SOCIAL_ADDON_PATH', str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, dirname(__FILE__) . '/addons'));
 
