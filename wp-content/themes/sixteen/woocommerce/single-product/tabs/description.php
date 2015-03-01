0b22cfa52f56d342b6d77fdfa9f89c6614c94ed0
Francisco de Juan <francisco.juan@redtux.io>
2015-03-01 08:11:24 +0100

    update theme sixteen to version 1.3.0.4

--- wp-content/themes/sixteen/woocommerce/single-product/tabs/description.php
+++ wp-content/themes/sixteen/woocommerce/single-product/tabs/description.php
@@ -14,6 +14,4 @@ global $woocommerce, $post;
 $heading = esc_html( apply_filters( 'woocommerce_product_description_heading', __( 'Product Description', 'woocommerce' ) ) );
 ?>
 
-<h2><?php //echo $heading; ?></h2>
-
 <?php the_content(); ?>
