a123f0349e9829972c16e470cd2f7a0084d516d9
Francisco de Juan <francisco.juan@redtux.io>
2015-07-05 11:34:14 +0200

    update sixteen theme to version 1.3.0.6

--- wp-content/themes/sixteen/woocommerce/single-product/tabs/description.php
+++ wp-content/themes/sixteen/woocommerce/single-product/tabs/description.php
@@ -11,7 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $woocommerce, $post;
 
-$heading = esc_html( apply_filters( 'woocommerce_product_description_heading', __( 'Product Description', 'woocommerce' ) ) );
 ?>
 
 <?php the_content(); ?>
